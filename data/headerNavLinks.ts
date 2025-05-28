65b9b0faaa208b28ff1e130f1667364d54dd350c
Irving Zamora <irvingzamora28@gmail.com>
2025-05-28 07:09:52 -0600

    feat: replace pricing link with blog link in navigation header

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
   { href: '#features', title: 'Features' },
-  { href: '#pricing', title: 'Pricing' },
   { href: '#contact', title: 'Contact' },
 ]
 
